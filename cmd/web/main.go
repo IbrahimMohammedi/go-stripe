d5bfdb1ff3e67ffdc301b15342cfe305606e0170
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-10-09 12:23:38 +0100

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+type config struct {
+	port int
+	env  string
+	api  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+}
+
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	// might have an error here
+	templateCache map[string]*template.Template
+	version       string
+}
+
+// router function
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port), // listen on port 4000 for requests
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+	app.infoLog.Println("Statring server in %s modeo on port %d", app.config.env, app.config.port)
+	return srv.ListenAndServe()
+}
+func main() {
+	var cfg config
+	// populate the var
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application enviornment {development|production}")
+	flag.StringVar(&cfg.api, "apy", "http://localhost:4001", "URL to api")
+
+	flag.Parse()
+	// reading stripe keys
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
+	// setting up the loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// map for template cache
+	tc := make(map[string]*template.Template)
+
+	// apllication variable
+	app := &application{
+		config:        cfg,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
+		templateCache: tc,
+		version:       version,
+	}
+	err := app.serve()
+	if err != nil {
+		app.errorLog.Println(err)
+		log.Fatal(err)
+	}
+}
